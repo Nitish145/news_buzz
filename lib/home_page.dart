67d00b5c729e3a8a462f52e6c4c008e5cba411b3
Nitish Aggarwal <royalnitish21@gmail.com>
2019-07-14 19:36:48 +0530

    feat: Add BottomNavigationBar in home_page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:news_buzz/authentication.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final BaseAuth auth;
   final VoidCallback onSignedOut;
 
   const HomePage({Key key, this.auth, this.onSignedOut}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _currentIndex = 0;
+
   void _signOut() async {
     try {
-      await auth.signOut();
-      onSignedOut();
+      await widget.auth.signOut();
+      widget.onSignedOut();
     } catch (e) {
       print(e);
     }
@@ -21,23 +28,86 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("NewsBuzz"),
-        backgroundColor: Theme.of(context).primaryColor,
+        backgroundColor: Theme
+            .of(context)
+            .primaryColor,
         actions: <Widget>[
           GestureDetector(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Center(
-                child: Text(
-                  "LogOut",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500),
-                ),
-              ),
+                  child: CircleAvatar(
+                      child: Image.asset("assets/images/logout_icon.png"))),
             ),
             onTap: _signOut,
           )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme
+                .of(context)
+                .primaryColor,
+            icon: Icon(
+              Icons.menu,
+              size: 30,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Home",
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme
+                .of(context)
+                .primaryColor,
+            icon: Icon(
+              Icons.apps,
+              size: 30,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Cateogries",
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme
+                .of(context)
+                .primaryColor,
+            icon: Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Search",
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme
+                .of(context)
+                .primaryColor,
+            icon: Icon(
+              Icons.bookmark,
+              size: 30,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Bookmarked",
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ),
         ],
       ),
     );
